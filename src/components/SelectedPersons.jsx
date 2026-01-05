a1a6b71758e3ae32eeadfdacb0571ddbc6370cfb
Md Ziaus Samad <mzsbulbul@gmail.com>
2026-01-05 14:47:12 +0600

    Refactor SelectedPersons component formatting

--- src/components/SelectedPersons.jsx
+++ src/components/SelectedPersons.jsx
@@ -3,23 +3,23 @@ import { PersonContext } from '../contexts/Person';
 import Headline from './Headline';
 
 const SelectedPersons = () => {
-    const { selectedPersons } = useContext(PersonContext);
+  const { selectedPersons } = useContext(PersonContext);
 
-    return (
-        <div className='mt-5 max-w-md mx-auto'>
-            <Headline
-                as='h3'
-                text='Selected Persons'
-                className='font-bold text-2xl mb-5 text-white bg-sky-500 p-4 text-center rounded'
-            />
+  return (
+    <div className='mt-5 max-w-md mx-auto'>
+      <Headline
+        as='h3'
+        text='Selected Persons'
+        className='font-bold text-2xl mb-5 text-white bg-sky-500 p-4 text-center rounded'
+      />
 
-            <ul className='grid grid-cols-2 list-disc list-inside'>
-                {selectedPersons.map((person, index) => (
-                    <li key={index}>{person}</li>
-                ))}
-            </ul>
-        </div>
-    );
+      <ul className='grid grid-cols-2 list-disc list-inside'>
+        {selectedPersons.map((person, index) => (
+          <li key={index}>{person}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default SelectedPersons;
